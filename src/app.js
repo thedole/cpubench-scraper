2c989feac288353616e480a965de7855764bcccc
Dag Ole Kristoffersen <thedole@gmail.com>
2015-11-08 20:42:02 +0100

    Promisyfied the shit out ouf it

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 ﻿// cpubench hack to find the good buys
 // cputable headers Array.prototype.map.call(cputable.rows[0].cells, function (cell) { return cell.innerText; });
 // ["CPU Name", "Passmark CPU Mark (higher is better)", "Rank (lower is better)", "CPU Value (higher is better)", "Price (USD)"]
-var cpubench = require('./cpubench.js'),
+var cpubench = require('./cpubench.js')('http://www.cpubenchmark.net/cpu_list.php'),
     cpulist = require('./cpulist.js');
 
-    cpubench.getElementById('cputable', function (err, cputable) {
-        if (!err) {
-            var cpubenchlist = cpulist(cputable);
-            console.log(cpubenchlist.header());
-        } else {
-            console.log(err);
-        }
+cpubench.getElementById('cputable')
+    .then(function (cputable) {
+        var cpubenchlist = cpulist(cputable);
+        console.log(cpubenchlist.header());
+    })
+    .catch(function (err) {
+        console.log(err);
     });
 
--- src/cpubench.js
+++ src/cpubench.js
@@ -2,24 +2,23 @@
 // cputable headers Array.prototype.map.call(cputable.rows[0].cells, function (cell) { return cell.innerText; });
 // ["CPU Name", "Passmark CPU Mark (higher is better)", "Rank (lower is better)", "CPU Value (higher is better)", "Price (USD)"]
 var rp = require('request-promise'),
-    domino = require('domino');
+    domino = require('domino'),
+    Promise = require('bluebird');
 
-module.exports = (function (url) {
+module.exports = function (url) {
     return {
-        getElementById: function (id, callback) {
-            rp(url)
+        getElementById: function (id) {
+            return rp(url)
                 .then(function (htmlstring) {
                 var document = domino.createDocument(htmlstring);
-                    var element = document.querySelector('#' + id);
-                    callback(null, element);
-            })
-            .catch(function (err) {
-                console.log(err);
+                var element = document.querySelector('#' + id);
+                
+                return Promise.resolve(element);
             });
         },
         setUrl: function (newUrl){
             url = newUrl;
         }
     }
-})('http://www.cpubenchmark.net/cpu_list.php');
+};
 
